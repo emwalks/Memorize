d52fc0014d66b634cce9f1f6b0269d1610057fa2
Emma Walker <walkere@tcd.ie>
2023-01-20 08:28:23 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
