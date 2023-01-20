7b2856acb3e4c860abe61a03b9ee6afcc5c5f1c0
Emma Walker <walkere@tcd.ie>
2023-01-20 11:12:20 +0000

    Adds different form factor devices to previews

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -21,6 +21,17 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+//            ContentView()
+//                .previewDevice("iPhone 8")
+//                .previewDisplayName("iPhone 8")
+//                .environment(\.sizeCategory, .accessibilityLarge)
+            ContentView()
+                .previewDevice("iPhone 13 mini")
+                .previewDisplayName("iPhone 13 mini")
+//            ContentView()
+//                .previewDevice("iPhone 14 Pro Max")
+//                .previewDisplayName("iPhone 14 Pro Max")
+        }
     }
 }
